dfca91628a533a3e5694648734e40bc8bba02079
Lammatian <mateusz.parafinski@gmail.com>
2019-01-22 23:52:36 +0000

    Implemented AdaptiveCounter with all the underlying logic. Comparison of (Top, Top) to be determined

--- src/solvers/sspm.ml
+++ src/solvers/sspm.ml
@@ -57,19 +57,87 @@ module BString = struct
     | BString arr, Empty -> if arr.(0) then 1 else -1
     | (BString arr1 as b1), (BString arr2 as b2) ->
       let i = ref 0 in
-      let j = ref 0 in
-      let ret = ref None in
-      while Option.is_none !ret && !i < length b1 && !j < length b2 do 
-        if arr1.(!i) && not arr2.(!j) then ret := Some 1
-        else if not arr1.(!i) && arr2.(!j) then ret := Some (-1)
+      let ret = ref 0 in
+      while !ret == 0 && !i < length b1 && !i < length b2 do 
+        if arr1.(!i) && not arr2.(!i) then ret := 1
+        else if not arr1.(!i) && arr2.(!i) then ret := -1
         else
           i := !i + 1;
-          j := !j + 1;
       done;
-      match !ret with
-      | None -> 0
-      | Some x -> x
+      !ret
 end
+
+module AdaptiveCounter = struct
+  type t = ACounter of BString.t array | Top
+
+  let create list = ACounter (Array.of_list list)
+
+  let createTop () = Top
+
+  let length = function
+    | Top -> failwith "Length called on Top Adaptive Counter"
+    | ACounter arr -> Array.length arr
+
+  let trim ac n =
+    match ac with
+    | Top -> Top
+    | ACounter arr -> ACounter (Array.sub arr 0 (Array.length arr - n))
+
+  let compare ac1 ac2 =
+    match ac1, ac2 with
+    | Top, Top -> 1 (** TODO: Check this **)
+    | Top, ACounter arr -> 1
+    | ACounter arr, Top -> -1
+    | (ACounter arr1 as ac1), (ACounter arr2 as ac2) ->
+    let i = ref 0 in
+    let ret = ref 0 in
+    while !ret == 0 && !i < length ac1 && !i < length ac2 do
+      ret := BString.compare arr1.(!i) arr2.(!i);
+      i := !i + 1
+    done;
+    !ret
+
+  let append ac bstr = 
+    match ac with
+    | Top -> failwith "Append called on Top Adaptive Counter"
+    | ACounter arr -> ACounter (Array.append arr [|bstr|])
+
+  let getLast = function
+    | Top -> failwith "GetLast called on Top Adaptive Counter"
+    | ACounter arr -> arr.(Array.length arr - 1)
+
+  let isMax = function
+    | Top -> true
+    | ACounter _ -> false
+
+  let show = function
+    | Top -> "T"
+    | ACounter arr ->
+      let list = Array.to_list arr in
+      let rec loop = function
+        | [] -> ""
+        | [x] -> BString.show x
+        | hd :: tl -> BString.show hd ^ "," ^ loop tl
+      in
+      "(" ^ loop list ^ ")"
+
+  let print ac = show ac |> print_string
+end
+
+module ProgressMeasure = struct
+  type t = (Paritygame.node, AdaptiveCounter.t) Hashtbl.t
+
+  let create nodes = Hashtbl.create (Paritygame.ns_size nodes)
+
+  let lift pm node = ()
+
+  let getAC pm node = Hashtbl.find pm node
+
+  let getWinningSet pm = Paritygame.sol_make 0
+
+  let getWinningStrategy pm = Paritygame.str_make 0
+end
+
 
 let solve (game : Paritygame.paritygame) = (sol_make 0, str_make 0)
 
--- src/solvers/sspm.mli
+++ src/solvers/sspm.mli
@@ -28,3 +28,45 @@ module BString : sig
       1 if first is bigger than the second one **)
   val compare   : t -> t -> int
 end
+
+module AdaptiveCounter : sig
+  type t
+  (** Create AdaptiveCounter from list of BStrings **)
+  val create    : BString.t list -> t
+  (** Create Top AdaptiveCounter **)
+  val createTop : unit -> t
+  (** Get length of the AdaptiveCounter defined as
+      the number of BStrings inside it **)
+  val length    : t -> int
+  (** Trim the AdaptiveCounter of the last n entries **)
+  val trim      : t -> int -> t
+  (** Compare two AdaptiveCounters with each other,
+      returning -1 if first is smaller, 0 if they're
+      equal and 1 if the first is bigger **)
+  val compare   : t -> t -> int
+  (** Append a BString to the end of the AdaptiveCounter **)
+  val append    : t -> BString.t -> t
+  (** Get last item of the AdaptiveCounter **)
+  val getLast   : t -> BString.t
+  (** Determine if the AdaptiveCounter is the top element **)
+  val isMax     : t -> bool
+  (** Returns the string representation of the AdaptiveCounter **)
+  val show      : t -> string
+  (** Prints the BString to the standard output **)
+  val print     : t -> unit
+end
+
+module ProgressMeasure : sig
+  type t
+  (** Create ProgressMeasure mapping all nodes to
+      the lowest possible AdaptiveCounter **)
+  val create             : Paritygame.nodeset -> t
+  (** Lift method for making the given node progressive **)
+  val lift               : t -> Paritygame.node -> unit
+  (** Get AdaptiveCounter for the given node **)
+  val getAC              : t -> Paritygame.node -> AdaptiveCounter.t
+  (** Get the winning set for the given ProgressMeasure **)
+  val getWinningSet      : t -> Paritygame.solution
+  (** Get the winning strategy for the given ProgressMeasure **)
+  val getWinningStrategy : t -> Paritygame.strategy
+end
