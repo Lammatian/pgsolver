fae9dd18102eac215b4b938241d4bf3c8bee8760
Lammatian <mateusz.parafinski@gmail.com>
2019-02-10 19:19:23 +0000

    Implemented conver function. It hasn't been tested yet at all. Also, a lot of refactoring needs to be done as the size of both the convert function as well as the number of helpers has blown up and it's hard to wrap one head around it already

--- src/solvers/registergame.ml
+++ src/solvers/registergame.ml
@@ -19,14 +19,92 @@ module RegisterGame = struct
 
   let k = ref 0
 
+  let p = ref 0
+
+  let idx_to_regs i =
+    (** Size of the domain for each register **)
+    let rd = !p + 1 in
+    let rec conv l x =
+      if x = 0 then
+        (** Finished, add trailing zeros to the result
+            to make it of length s **)
+        let len = !k - List.length l in
+        let trailing_zeros = Array.make len 0 in
+        (** TODO: Is this reversing really necessary or
+            could we avoid that? **)
+        let rev_list = List.rev l in
+        Array.append (Array.of_list rev_list) trailing_zeros
+      else
+        (** Add x % s to the beginning of the list and
+            divide x by s **)
+        conv ((x mod rd) :: l) (x / rd) 
+    in
+    conv [] i
+
+  let regs_to_idx regs =
+    let rd = !p + 1 in
+    let result = ref 0 in
+    let mult = ref 1 in
+    for i = 0 to Array.length regs - 1 do
+      result := regs.(i) * !mult;
+      mult := !mult * rd;
+    done;
+    !result
+
+  let regs_to_idx_reset regs r =
+    (** Size of the domain for each register **)
+    let rd = !p + 1 in
+    let result = ref 0 in
+    (** Start with rd as multiplier because when resetting, we
+        are starting from looking at the second register, first
+        is 0 **)
+    let mult = ref rd in
+    for i = 0 to Array.length regs - 1 do
+      if i <> r - 1 then
+        result := regs.(i) * !mult;
+        mult := !mult * rd;
+    done;
+    !result
+
+  let desc_to_idx node regs t =
+    let reg_idx = regs_to_idx regs in
+    let node_idx = node * 2 * !s in
+    let t_idx = if t = 1 then !s else 0 in
+    node_idx + reg_idx + t_idx
+
+  let desc_to_idx_reset node regs r =
+    let reg_idx = regs_to_idx_reset regs r in
+    let node_idx = node * 2 * !s in
+    let t_idx = !s in
+    node_idx + reg_idx + t_idx
+
+  let idx_to_node i = i / (2 * !s)
+
+  let idx_to_t i = if i mod (2 * !s) >= !s then 1 else 0
+
+  let idx_to_desc i = 
+    let v = idx_to_node i in
+    let registers = idx_to_regs (i mod !s) in
+    let t = idx_to_t in
+    (v, registers, t)
+
+  let update_registers regs p =
+    Array.map (fun r -> max r p) regs
+
+  let get_rg_neighbour i neighbour_in_g pg =
+    let registers = idx_to_regs i in 
+    let priority_in_g = Paritygame.pg_get_priority pg neighbour_in_g in
+    let updated_registers = update_registers registers priority_in_g in
+    desc_to_idx neighbour_in_g updated_registers 0
+
   let convert pg =
     let module PG = Paritygame in
     let nodes = PG.collect_nodes pg (fun _ _ -> true) in
     let n = PG.ns_size nodes in
     let n_float = float_of_int n in
     k := (log2 n_float) +. 1. |> ceil |> int_of_float;
-    let max_priority = PG.pg_max_prio pg in
-    s := pow max_priority !k;
+    p := PG.pg_max_prio pg;
+    s := pow !p !k;
     (** TODO: Idea - mark with 'x' in description if a node has been
         visited, to then easily remove nodes with in-degree 0 **)
     (** TODO: Making separate HUGE arrays and then combining them
@@ -43,38 +121,60 @@ module RegisterGame = struct
         rg_nodes.(j) <- (priority, owner, [], None)
       done;)
       nodes;
-    pg
+    (** Reset node creation with appropriate successors (one for each) **)
+    let reset_nodes = Array.make (Array.length rg_nodes * !k) (0, PG.plr_undef, [], None) in
+    Array.iteri (fun i _ ->
+      for r = 1 to !k do
+        (** Even owns all reset nodes **)
+        let owner = PG.plr_Even in
+        (** Determine the register contents of the node i **)
+        let registers = idx_to_regs i in
+        let priority = if registers.(r - 1) mod 2 = 0 then 2*r else 2*r + 1 in
+        (** Get the successor after reset **)
+        let underlying_node = idx_to_node i in
+        let successor = desc_to_idx_reset underlying_node registers r in
+        reset_nodes.(i * !k + r) <- (priority, owner, [successor], None)
+      done)
+      rg_nodes;
+    (** Adding successors to all non-reset nodes **)
+    let rg_nodes = Array.mapi (fun i (prio, owner, succ, desc) ->
+      let t = idx_to_t i in 
+      let successors = succ in
+      let node_in_g = idx_to_node i in
+      if t = 0 then (** Add skip edge and reset as neighbour **)
+        (** Skip edge **)
+        let skip = i + !s in
+        (** Each possible register reset is a neighbour **)
+        let rec get_reset_edges successors r =
+          if r > !k then successors
+          else get_reset_edges ((i * !k + (r - 1)) :: successors) (r + 1)
+        in
+        let successors = skip :: get_reset_edges successors 1 in
+        (prio, owner, successors, desc)
+      else
+        (** Get all successors of the current node in G **)
+        let successors_in_g = PG.pg_get_successors pg node_in_g in
+        (** Determine all successors in RG **)
+        let successors = PG.ns_fold (fun acc node ->
+          (get_rg_neighbour i node pg) :: acc)
+          [] successors_in_g
+        in
+        (prio, owner, successors, desc)
+      )
+      rg_nodes 
+    in
+    (** Initialise the game **)
+    PG.pg_init (Array.length rg_nodes + Array.length reset_nodes)
+      (fun node ->
+        if node < Array.length rg_nodes then
+          rg_nodes.(node)
+        else
+          reset_nodes.(node - Array.length rg_nodes)
+      )
 
   let recover_sol rg = Paritygame.sol_make 0
 
   let recover_str rg = Paritygame.str_make 0
-
-  let get_register_contents i =
-    let rec conv l x =
-      if x = 0 then
-        (** Finished, add trailing zeros to the result
-            to make it of length s **)
-        let len = !k - List.length l in
-        let trailing_zeros = Array.make len 0 in
-        (** TODO: Is this reversing really necessary or
-            could we avoid that? **)
-        let rev_list = List.rev l in
-        Array.append (Array.of_list rev_list) trailing_zeros
-      else
-        (** Add x % s to the beginning of the list and
-            divide x by s **)
-        conv ((x mod !s) :: l) (x / !s) 
-    in
-    conv [] i
-
-
-  let idx_to_node i = i / (2 * !s)
-
-  let idx_to_desc i = 
-    let v = idx_to_node i in
-    let registers = get_register_contents (i mod !s) in
-    let t = if i mod (2 * !s) >= !s then 1 else 0 in
-    (v, registers, t)
 end
 
 let solve game = 
--- src/solvers/registergame.mli
+++ src/solvers/registergame.mli
@@ -21,6 +21,69 @@ module RegisterGame : sig
   (** Number of registers **)
   val k : int ref
 
+  (** Max priority in the game and consequently |I| - 1 **)
+  val p : int ref
+
+  (** Determine the register contents from an integer
+      that the contents are stored as. The register 
+      contents are just an s-ary representation of the
+      given number:
+      
+      idx = x_1 + s * x_2 + s^2 * x_3 + ... + s^(n-1) * x_n **)
+  val idx_to_regs : Paritygame.node -> int array
+
+  (** Given register contents, determine the unique index
+      of those contents. This is an inverse operation to
+      [idx_to_regs] **)
+  val regs_to_idx : int array -> Paritygame.node
+
+  (** Given register contents and the index of the register
+      being reset, determine the index of the resulting
+      register contents **)
+  val regs_to_idx_reset : int array -> int -> Paritygame.node
+
+  (** Given a description of a register game node in the form
+      [underlying node], [registers], [t], retrieve the index 
+      of it **)
+  val desc_to_idx : Paritygame.node -> int array -> int -> int
+
+  (** Given a node from the underlying game, the register
+      contents and the register to reset, give the index of
+      the resulting node in the register game. The value of
+      t is not needed as the resulting node will have t = 1
+      always **)
+  val desc_to_idx_reset : Paritygame.node -> int array -> int -> int
+
+  (** A map from the index of a register game node to
+      the underlying node in the original game
+      
+      TODO: Describe the mapping here or below **)
+  val idx_to_node : Paritygame.node -> int
+
+  (** Recover the value of t (0 or 1) from the index **)
+  val idx_to_t : Paritygame.node -> int
+
+  (** A map from the index of a register game node to
+      its description, i.e. (v, x, t), where
+      v is the vertex from the original game
+      x is the content of the registers
+      t is the indicator for reset/move **)
+  val idx_to_desc : Paritygame.node -> int * int array * int
+
+  (** Given registers and a priority of a new node to
+      visit, update each register to hold the max value
+      between it's original content and the priority **)
+  val update_registers : int array -> int -> int array
+
+  (** Given an index of a current node and its neighbour
+      in the original game, determine the neighbour in the
+      register game
+      i.e. if current node is (v, x, 1) and v' is a neighbour
+      of v in the original game, then we return the index
+      of (v', x', 0), where x' are the updated registers after
+      moving to a new vertex **)
+  val get_rg_neighbour : int -> Paritygame.node -> int
+
   (** Given a paritygame , convert to a register game
       with priorities bounded by log(n) + 1 where n is
       the number of nodes in the original game **)
@@ -33,25 +96,4 @@ module RegisterGame : sig
   (** Recover a strategy for the original game from the 
       solved register game **)
   val recover_str : t -> Paritygame.strategy
-
-  (** Determine the register contents from an integer
-      that the contents are stored as. The register 
-      contents are just an s-ary representation of the
-      given number:
-      
-      idx = x_1 + s * x_2 + s^2 * x_3 + ... + s^(n-1) * x_n **)
-  val get_register_contents : Paritygame.node -> int array
-
-  (** A map from the index of a register game node to
-      the underlying node in the original game
-      
-      TODO: Describe the mapping here or below **)
-  val idx_to_node : Paritygame.node -> int
-
-  (** A map from the index of a register game node to
-      its description, i.e. (v, x, t), where
-      v is the vertex from the original game
-      x is the content of the registers
-      t is the indicator for reset/move **)
-  val idx_to_desc : Paritygame.node -> int * int array * int
 end
